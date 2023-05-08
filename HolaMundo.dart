c107ba9559347cf52a5a669df635c6eec9391849
Fabian Ramirez <fabianrm@gmail.com>
2023-05-08 18:07:46 -0500

    Hola mundo en dart...primer commit

---
+++ HolaMundo.dart
@@ -0,0 +1,11 @@
+void main() {
+  final String myName = 'Fabián';
+  print('Hola $myName');
+  print('Hola ${myName.toLowerCase()}');
+  print (sumar(3, 5));
+}
+
+int sumar(int n1, int n2) {
+  return n1 + n2;
+}
+
