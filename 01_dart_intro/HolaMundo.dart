2d7aff48b73bdf9fea2be07b7ce0aa06585383d5
Fabian Ramirez <fabianrm@gmail.com>
2023-05-08 18:13:15 -0500

    Intro dart

--- 01_dart_intro/HolaMundo.dart
+++ 01_dart_intro/HolaMundo.dart
@@ -2,10 +2,10 @@ void main() {
   final String myName = 'Fabián';
   print('Hola $myName');
   print('Hola ${myName.toLowerCase()}');
-  print (sumar(3, 5));
+  print(sumar(3, 5));
+  print('Sumando => ${3 + 3}');
 }
 
 int sumar(int n1, int n2) {
   return n1 + n2;
 }
-
