6f1dd0b8cab72efd31eb44b2ec6860ebab6cacf6
Fabian Ramirez <fabianrm@gmail.com>
2024-01-05 11:44:20 -0500

    Estructura incial de cinemaapp

---
+++ 03_app_movies/cinemapedia/lib/config/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  //Configuracion tema inicial
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0xff2862f5),
+      );
+}
